7e2d4516d228334ceec22bae46e102444bceb800
Dmytro_Haiduchyk <dimych.g.m@gmail.com>
2019-12-03 01:10:06 +0200

    Animation fixed. Total panel done

---
+++ Assets/Scripts/Core/StudentTotalPanel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Journal_Model.Core
+{
+    public class StudentTotalPanel : MonoBehaviour
+    {
+        public RectTransform Rect;
+        [SerializeField] private Text Name;
+        [SerializeField] private Text LessonsText;
+        [SerializeField] private Text ExtraPointsText;
+        [SerializeField] private Text ModuleControlWorkText;
+        [SerializeField] private Text TotalText;
+        
+        public void Init(string name, string lessons, string extra, string module, string total)
+        {
+            Name.text = name;
+            LessonsText.text = lessons;
+            ExtraPointsText.text = extra;
+            ModuleControlWorkText.text = module;
+            TotalText.text = total;
+        }
+    }
+}
---
+++ Assets/Scripts/Core/TotalPanel.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Journal_Model;
+using Journal_Model.Core;
+using Journal_Model.Lesson;
+using UI_Controllers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TotalPanel : MonoBehaviour
+{
+    [SerializeField] private JournalModelProxy Proxy;
+    [SerializeField] private RectTransform Content;
+    [SerializeField] private StudentTotalPanel Prefab;
+    private List<StudentTotalPanel> Students;
+
+    public void OnEnable()
+    {
+        if (Students != null)
+        {
+            foreach (StudentTotalPanel panel in Students)
+            {
+                Destroy(panel.gameObject);
+            }
+        }
+
+        float totalHeight = 0;
+        Students = new List<StudentTotalPanel>();
+        foreach (StudentSO student in Proxy.Students)
+        {
+            float lessonsPoints = 0;
+            foreach (Lesson lesson in Proxy.JournalModel.Lessons)
+            {
+                LessonData lessonData = lesson.Datas[student];
+                foreach (PointData data in lessonData.Points.Keys)
+                {
+                    lessonsPoints += lessonData.Points[data];
+                }
+            }
+
+            float extraPoints = 0;
+            if (Proxy.JournalModel.ExtraPointsData.Points.ContainsKey(student))
+            {
+                Dictionary<PointData, float> extra = Proxy.JournalModel.ExtraPointsData.Points[student];
+                foreach (PointData data in extra.Keys)
+                {
+                    extraPoints += extra[data];
+                }
+            }
+
+            float modulePoints = 0;
+            if (Proxy.JournalModel.ModulePointsData.Points.ContainsKey(student))
+            {
+                Dictionary<PointData, float> module = Proxy.JournalModel.ModulePointsData.Points[student];
+                foreach (PointData data in module.Keys)
+                {
+                    modulePoints += module[data];
+                }
+            }
+
+            StudentTotalPanel panel = Instantiate(Prefab, Content);
+            panel.Init(student.ToShortNSP(), 
+                lessonsPoints.ToString(), 
+                extraPoints.ToString(),
+                modulePoints.ToString(), 
+                (lessonsPoints + extraPoints + modulePoints).ToString());
+
+            Vector2 position = panel.Rect.anchoredPosition;
+            position.y = -totalHeight;
+            panel.Rect.anchoredPosition = position;
+            totalHeight += panel.Rect.sizeDelta.y;
+            Students.Add(panel);
+        }
+
+        Vector2 size = Content.sizeDelta;
+        size.y = totalHeight;
+        Content.sizeDelta = size;
+    }
+}
